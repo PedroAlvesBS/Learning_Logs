fe70f80fa8a328bc01129b31aee4b8355a084f71
Pedro <pepedro2512@gmail.com>
2023-02-06 22:12:28 -0300

    Make modifications to allow connections

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!1mkm!wcct=7b((&k0i0wch87f7s54xbq8i%uz%)3=l4wndv)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOGIN_URL = 'users:login'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
